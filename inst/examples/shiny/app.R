19da18438caf292ae34b8f861a15949a1bf5827a
DataSciBurgoon <burgoon.lyle@gmail.com>
2016-03-03 21:31:10 -0500

    Added some test data and modified the example

--- inst/examples/shiny/app.R
+++ inst/examples/shiny/app.R
@@ -6,18 +6,40 @@ json_aopns <- fromJSON("http://localhost:3000/networks")
 #### Server ####
 server <- function(input, output, session) {
 
-    observeEvent(input$go, {
-      print(input$network)
-      network_scrubbed <- gsub(" ", "%20", isolate(input$network))
-      print(network_scrubbed)
-      aopns <- fromJSON(paste("http://localhost:3000/networks/", network_scrubbed, sep=""))
-      output$force <- renderAop({
-        print(aopns)
+  aopns <- NULL
+
+  observeEvent(input$go, {
+    print(input$network)
+    network_scrubbed <- gsub(" ", "%20", isolate(input$network))
+    print(network_scrubbed)
+    aopns <- fromJSON(paste("http://localhost:3000/networks/", network_scrubbed, sep=""))
+    aopns$nodes <- data.frame(name=aopns$nodes, group=1)
+    output$force <- renderAop({
+      #This catches the input file
+      inFile <- input$file1
+      if(is.null(inFile)){
+        print("yo!")
+        #This sends the info to the UI to render the AOP
         aopxplorer(Links = aopns$links, Nodes = aopns$nodes,
                    Source = "source", Target = "target", Value = 1,
-                   NodeID = "name", Group = 1, opacity = 100)
-      })
+                   NodeID = "name", Group = "group", opacity = 100)
+      }
+      else{
+        #This reads in the input data file
+        data_with_group <- read.csv(inFile$datapath, header=TRUE, sep="\t")
+
+        #This updates the group assignments
+        #The sorts are VERY important to make sure everything gets mapped to the right place
+        #This does not allow for incomplete input files -- there needs to be a line for every node!!
+        aopns$nodes <- data_with_group[match(aopns$nodes$name, data_with_group$name),]
+        print(aopns$nodes)
+        aopxplorer(Links = aopns$links, Nodes = aopns$nodes,
+                   Source = "source", Target = "target", Value = 1,
+                   NodeID = "name", Group = "group", opacity = 100)
+      }
+
     })
+  })
 
 }
 
@@ -25,14 +47,16 @@ server <- function(input, output, session) {
 
 ui <- shinyUI(fluidPage(
 
-  titlePanel("Shiny AOPNetwork "),
+  tags$img(src="http://localhost:3000/modules/core/img/brand/aopxplorer_logo.png", width="25%", height="25%"),
+
+  titlePanel("Explore AOP Networks with Your Data"),
 
   sidebarLayout(
     sidebarPanel(
       selectInput('network', 'Choose AOP Network', json_aopns$ao_label$value),
-      actionButton("go", "Go!"),
-      fileInput('file1', 'Choose CSV File', accept=c('text/csv',
-                                                     'text/comma-separated-values,text/plain', '.csv'))
+      fileInput('file1', 'Choose CSV File [optional]', accept=c('text/csv',
+                                                     'text/comma-separated-values,text/plain', '.csv')),
+      actionButton("go", "Go!")
     ),
     mainPanel(
       tabsetPanel(
@@ -43,4 +67,8 @@ ui <- shinyUI(fluidPage(
 ))
 
 #### Run ####
-shinyApp(ui = ui, server = server)
+app <- shinyApp(ui = ui, server = server)
+runApp(app, port=7600)
+
+
+
